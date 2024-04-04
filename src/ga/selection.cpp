1d83ef18c81fa0df9a5021b97050d314c87158e8
Dwahgon <therealdragonofwar@gmail.com>
2024-04-04 15:48:54 -0300

    fix: avoid selecting the same individual in a pair for crossover

--- src/ga/selection.cpp
+++ src/ga/selection.cpp
@@ -1,39 +1,31 @@
 #include "selection.h"
 
-#include <cctype>
-#include <vector>
-#include <algorithm>
-#include <random>
-
-std::size_t binSearchCDF(const std::vector<double>& l, double n){
-    if (n >= 1.0) return l.size() - 1;
-    if (n <= 0.0) return 0;
-    std::size_t iStart {0}, iEnd {l.size() - 1}, curI {(iStart + iEnd) / 2 };
-
-    while (!((curI == 0 ? 0.0 : l.at(curI - 1)) < n && n <= l.at(curI) ))
-    {
-        if (n < l.at(curI)){
-            iEnd = curI;
-        }else{
-            iStart = curI + 1;
-        }
-        curI = (iStart + iEnd) / 2;
-    }
-    return curI;
-}
-
 std::vector<int> ga::rouletteWheelSelection(std::mt19937& rng, std::size_t amount, ga::Scores scores){
-    std::uniform_real_distribution<double> dist(0.0, 1.0);
     // Calculate total population score
-    int totalScore = 0;
+    std::uniform_real_distribution<double> dist(0.0, 1.0);
+    int totalScore {0};
     for (auto& n : scores) totalScore += n;
 
     // Calculate CDF
-    std::vector<double> cdfScores;
-    for (std::size_t i {0}; i < scores.size(); i++) cdfScores.push_back((!cdfScores.size() ? 0 : cdfScores.at(i-1)) + (double)scores.at(i) / (double)totalScore);
-
+    Score scoreAcc;
+    std::size_t lastScoreI {scores.size()};
+    double rn, // Random number
+        maxScoreAccRatio; // 1 / (Total score - score of lastScoreI)
     std::vector<int> selectedIndividuals;
-    while (amount--) selectedIndividuals.push_back(binSearchCDF(cdfScores, dist(rng)));
+    while (amount--){
+        rn = dist(rng);
+        scoreAcc = 0;
+        maxScoreAccRatio = 1.0 / (double)(totalScore - (lastScoreI == scores.size() ? 0 : scores.at(lastScoreI)));
+        for (std::size_t i {0}; i < scores.size(); i++){
+            if (i == lastScoreI) continue;
+            scoreAcc += scores.at(i);
+            if (rn < (double)scoreAcc * maxScoreAccRatio) {
+                selectedIndividuals.push_back(i);
+                lastScoreI = i;
+                break;
+            }
+        }
+    }
 
     return selectedIndividuals;
 }
